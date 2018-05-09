ca2fedcc096f8f2000fa3935008f7fd35e49f9d5
Joel Håkansson <joel.hakansson@mtm.se>
2018-05-09 14:00:15 +0200

    Adds a temp folder handler

--- src/org/daisy/streamline/engine/PathTools.java
+++ src/org/daisy/streamline/engine/PathTools.java
@@ -4,16 +4,53 @@ import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+import java.util.Random;
 
 /**
  * Provides path related tools.
  * @author Joel Håkansson
  */
 public final class PathTools {
+	private static final Random RND = new Random();
+	
 	private PathTools() {}
 	
+	/**
+	 * Creates a temporary folder.
+	 * @return the temporary folder
+	 * @throws IOException if a folder could not be created
+	 */
+	public static Path createTempFolder() throws IOException {
+		return createTempFolder("");
+	}
+
+	/**
+	 * Creates a temporary folder.
+	 * @param prefix a folder prefix
+	 * @return the temporary folder
+	 * @throws IOException if a folder could not be created
+	 * @throws NullPointerException if {@code prefix} is null
+	 */
+	public static Path createTempFolder(String prefix) throws IOException {
+		Path parent = Paths.get(System.getProperty("java.io.tmpdir"));
+		if (!Files.isDirectory(parent)) {
+			throw new IOException("java.io.tmpdir points to a non-existing folder: " + parent);
+		}
+		Path ret = null;
+		int i = 0;
+		do {
+			ret = parent.resolve(Objects.requireNonNull(prefix)+Long.toHexString(System.currentTimeMillis())+"-"+Integer.toHexString(RND.nextInt()));
+			i++;
+			if (i>=100) {
+				throw new IOException("Failed to create temporary folder.");
+			}
+		} while (!ret.toFile().mkdirs());
+		return ret;
+	}
 
 	/**
 	 * Deletes files and folders at the specified path, including the start path.
